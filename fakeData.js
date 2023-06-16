1a6ca36e66bc53368236ccb9690a737b6ab4f52e
Arthur Viegas <viegas.arthur2013@hotmail.com>
2023-06-16 15:05:04 -0300

    feat: adicionado novos usuarios, role e password

--- fakeData.js
+++ fakeData.js
@@ -2,12 +2,24 @@
 const fakeData  =  [
     {
         id: 1,
+        role: "user",
         name: "João Oliveira",
+        password: '$2a$12$CFm3PEhvAmYYjAa3Z2BYWuUV9DGXVATScX/XaUtzY1N0.WmD8HaEe', // senha123 - senha criptografada por bcrypt
         job: "Desenvolvedor"
-    },    {
+    },
+    {
         id: 2,
+        role: "admin",
         name: "Arthur",
+        password: '$2a$12$CFm3PEhvAmYYjAa3Z2BYWuUV9DGXVATScX/XaUtzY1N0.WmD8HaEe', // senha123 - senha criptografada por bcrypt
         job: "Desenvolvedor"
+    },
+    {
+        id: 3,
+        role: "user",
+        name: "Paulo",
+        password: '$2a$12$CFm3PEhvAmYYjAa3Z2BYWuUV9DGXVATScX/XaUtzY1N0.WmD8HaEe', // senha123 - senha criptografada por bcrypt
+        job: "Suporte"
     }
 ]
 
