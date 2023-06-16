8b15504983944e818b2c81cac22bd88d5cf77a13
Arthur Viegas <viegas.arthur2013@hotmail.com>
2023-06-15 22:26:47 -0300

    feat: implementa validações e atualização do usuario

--- teste4.js
+++ teste4.js
@@ -1,13 +1,33 @@
 var data =  require("./fakeData");
+function parametroStringValido(value) {
+    return typeof value === 'string' && value.trim() !== "";
+  }
 
 module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+    try {
+        const  id =  req.query.id;
+        const { name, job } = req.body;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+        // se qualquer propriedade estiver vazia e nao forem tipo string, retorna status 400.
+        if ((!parametroStringValido(name) && !parametroStringValido(job)) && (!id || id === '')) res.status(400).json('Parametros inválidos.');
 
-    res.send(reg);
+        // busca o usuario na lista.
+        const userIndex = data.findIndex(user => user.id === Number(id));
 
+        if (userIndex === -1) {
+          res.status(404).json("Usuário não encontrado.");
+        } else {
+            //utiliza o spread para manter todos os outros valores que não serão atualizados.
+          let updatedUser = {
+            ...data[userIndex],
+            name: name,
+            job: job
+          };
+          // reatribui o usuario com o updatedUser na posição do index.
+          data[userIndex] = updatedUser;
+            return res.status(200).json(updatedUser);
+        }
+    } catch (error) {
+        
+    }
 };
