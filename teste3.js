85038e350d18468eed99ec0aae18fc5327e92c3c
Arthur Viegas <viegas.arthur2013@hotmail.com>
2023-06-16 09:36:00 -0300

    fix: corrigido tratamento de dado

--- teste3.js
+++ teste3.js
@@ -2,7 +2,7 @@ var data =  require("./fakeData");
 
 module.exports = function(req, res) {
     try {
-        const name =  req.query.name.toLowerCase().trim();
+        const name =  req.query.name;
 
         // valida se a query existe e não está vazia.
         if (!name || name === '') res.status(400).json('Parametros inválidos.');
@@ -12,7 +12,7 @@ module.exports = function(req, res) {
 
         // A função filter() remove o usuário do array data com base no nome fornecido
         // reatribuindo todos itens que não tenham o nome informado (removendo ele da lista).
-        data = data.filter((user) => user.name.toLowerCase().trim() !== name);
+        data = data.filter((user) => user.name.toLowerCase().trim() !== name.toLowerCase().trim());
 
         // Armazenado o tamanho atual da lista.
         const currLengh = data.length;
