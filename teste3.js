1598778df04fc3af245c0c226cd39b06dd953d7a
Arthur Viegas <viegas.arthur2013@hotmail.com>
2023-06-15 21:56:26 -0300

    feat: implementa filtro para remover item da lista.

--- teste3.js
+++ teste3.js
@@ -1,15 +1,27 @@
 var data =  require("./fakeData");
 
 module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+    try {
+        const name =  req.query.name.toLowerCase().trim();
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+        // valida se a query existe e não está vazia.
+        if (!name || name === '') res.status(400).json('Parametros inválidos.');
+
+        // usado uma const para armazenar permanentemente o tamanho da lista atual.
+        const prevLength = data.length;
+
+        // A função filter() remove o usuário do array data com base no nome fornecido
+        // reatribuindo todos itens que não tenham o nome informado (removendo ele da lista).
+        data = data.filter((user) => user.name.toLowerCase().trim() !== name);
+
+        // Armazenado o tamanho atual da lista.
+        const currLengh = data.length;
+
+        // Se o tamanho atual da lista for menor que o inicial, entende-se que um item foi removido.
+        if(currLengh < prevLength) res.status(200).json();
+
+        res.status(404).json("Usuario não encontrado.");
+    } catch (error) {
+        // next(error);
     }
-
-    res.send("success");
-
 };
