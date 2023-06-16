4a3a2625650ae70f5f2e40e3da8da16b5f43820b
Arthur Viegas <viegas.arthur2013@hotmail.com>
2023-06-15 21:56:06 -0300

    fix: melhorias nas validações e adiciona comentarios

--- teste2.js
+++ teste2.js
@@ -1,18 +1,20 @@
 var data =  require("./fakeData");
 
+// função que valida se os tipo das propriedades sao strings e nao vazias.
 function parametroStringValido(value) {
-    return typeof value === 'string' && value.trim() !== '';
+    return typeof value === 'string' && value.trim() !== "";
   }
 
 module.exports = function(req, res, next) {
     try {
       const { name, job } = req.body;
 
-      if (!parametroStringValido(name) || !parametroStringValido(job)) {
-        return res.status(400).json('Parametros inválidos.');
-      }
+      // se qualquer propriedade estiver vazia e nao forem tipo string, retorna status 400.
+      if (!parametroStringValido(name) && !parametroStringValido(job)) res.status(400).json('Parametros inválidos.');
       
+      // armazena o tamanha da array em uma const para gerar o Id do usuário.
       const currLength = data.length;
+
       const newUser = {
           id: currLength + 1,
           name: name.trim(),
@@ -20,13 +22,12 @@ module.exports = function(req, res, next) {
       }
   
       data.push(newUser);
-  
-      if (data.includes(newUser)) {
-        res.status(201).json(newUser);
-      } else {
-        res.status(400).json("Erro ao inserir usuário.");
-      }
+
+      if (data.includes(newUser)) res.status(201).json(newUser);
+
+       res.status(400).json("Erro ao inserir usuário.");
     } catch (error) {
       next(error);
     }
   };
+  
